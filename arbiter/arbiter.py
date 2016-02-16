b4284a93f4ecb1e0451e3eaff8c40092609a911c
Ilion Beyst <ilion.beyst@gmail.com>
2016-02-16 23:18:09 +0100

    improve everything

--- arbiter/arbiter.py
+++ arbiter/arbiter.py
@@ -2,8 +2,8 @@
 
 from itertools import chain
 from collections import defaultdict
-from functools import total_ordering
 from math import ceil, sqrt
+from functools import total_ordering
 from subprocess import Popen, PIPE
 import sys
 
@@ -31,14 +31,15 @@ def parse_fort(game, string):
 
 def parse_road(game, string):
     a, b = string.split(' ')
-    game.forts[a].neighbours.add(game.forts[b])
-    game.forts[b].neighbours.add(game.forts[a])
+    build_road(game, game.forts[a], game.forts[b])
 
 
 def parse_march(game, string):
     origin, target, owner, size, steps = string.split(' ')
-    March(game, game.forts[origin], game.forts[target],
-          game.players.get(owner), int(size), int(steps))
+    origin = game.forts[origin]
+    target = game.forts[target]
+    owner = game.players[owner]
+    origin.roads[target].march(target, owner, int(size), int(steps))
 
 
 def read_map(game, handle):
@@ -76,53 +77,94 @@ def show_fort(fort):
 
 
 def show_road(road):
-    return "{} {}".format(*road)
+    return "{} {}".format(*road.headed_to.keys())
 
 
-def show_march(march):
+def show_march(tup):
+    pos, origin, target, march = tup
     return "{} {} {} {} {}".format(
-        march.origin, march.target, show_player(march.owner),
-        march.size, ceil(march.remaining_steps))
+        origin, target, show_player(march.owner),
+        march.size, pos)
 
 
 def show_visible(game, forts):
-    visible_forts = set.union(*(fort.neighbours for fort in forts))
-    roads = set(unorder((fort, n)) for fort in forts for n in fort.neighbours)
-    marches = list(chain(*(game.roads[road].marches() for road in roads)))
+    roads = set(road for fort in forts for road in fort.roads.values())
+    forts = set(chain(*(road.headed_to.keys() for road in roads)))
+    marches = list(chain(*(road.marches() for road in roads)))
     return '\n'.join([
-        show_section(visible_forts, 'forts', show_fort),
+        show_section(forts, 'forts', show_fort),
         show_section(roads, 'roads', show_road),
         show_section(marches, 'marches', show_march)
         ])
 
 
-def unorder(tup):
-    return tuple(sorted(tup))
+class Road:
+    def __init__(self, fort1, fort2):
+        self.length = fort1.distance(fort2)
+        self.headed_to = {
+                fort1: [None]*(self.length*2+1),
+                fort2: [None]*(self.length*2+1),
+        }
+
+    def march(self, destination, owner, size, steps=None):
+        position = 2 * (steps or self.length)
+        if self.headed_to[destination][position]:
+            self.headed_to[destination][position].size += size
+        else:
+            march = March(self, owner, size)
+            self.headed_to[destination][position] = march
+
+    def step(self):
+        self.half_step()
+        self.half_step()
+
+    def half_step(self):
+        for marches in self.headed_to.values():
+            for i in range(len(marches)-1):
+                marches[i] = marches[i+1]
+            marches[-1] = None
+        self.resolve_encounters()
+
+    def resolve_encounters(self):
+        army = lambda ind, fort: self.headed_to[fort][ind]
+
+        fort1, fort2 = self.headed_to.keys()
+        for i in range(2*self.length+1):
+            positions = [(i, fort1), (-i-1, fort2)]
+            armies = [army(*pos) for pos in positions]
+            if not all(armies):
+                continue
+            if len(set(army.owner for army in armies)) == 1:
+                continue
+            deaths = min(army.size for army in armies)
+            for index, fort in positions:
+                army(index, fort).size -= deaths
+                if army(index, fort).size == 0:
+                    army(index, fort).die()
+                    self.headed_to[fort][index] = None
+
+    def marches(self):
+        endpoints = list(self.headed_to.keys())
+        for destination, origin in [endpoints, reversed(endpoints)]:
+            for steps, march in enumerate(self.headed_to[destination]):
+                if march:
+                    yield (origin, destination, ceil(steps/2), march)
 
 
-class UnorderedTupleDefaultDict(dict):
-    def __init__(self, factory):
-        self.factory = factory
-
-    def __missing__(self, key):
-        self[key] = self.factory(key)
-        return self[key]
-
-    def __getitem__(self, key):
-        return super().__getitem__(unorder(key))
-
-    def __setitem__(self, key, value):
-        return super().__setitem__(unorder(key), value)
+def build_road(game, fort1, fort2):
+    road = Road(fort1, fort2)
+    fort1.roads[fort2] = road
+    fort2.roads[fort1] = road
+    game.roads.append(road)
 
 
-@total_ordering
 class Fort:
     def __init__(self, game, name, x, y, owner, garrison):
         self.game = game
         self.name = name
         self.x = x
         self.y = y
-        self.neighbours = set()
+        self.roads = {}
         self.owner = owner
         self.garrison = garrison
         game.forts[name] = self
@@ -130,25 +172,11 @@ class Fort:
             owner.forts.add(self)
 
     def dispatch(self, neighbour, size):
-        if neighbour in self.neighbours:
+        road = self.roads.get(neighbour)
+        if road:
             size = min(size, self.garrison)
-            steps = self.distance(neighbour)
-            March(self.game, self, neighbour, self.owner, size, steps)
+            road.march(neighbour, self.owner, size)
             self.garrison -= size
-
-    def reinforce(self, march):
-        if self.garrison < 0:
-            self.garrison = 0
-        self.garrison += march.size
-        march.remove()
-
-    def besiege(self, march):
-        tmp = self.garrison
-        self.garrison -= march.size
-        march.kill_soldiers(tmp)
-        if self.garrison < 0:
-            march.owner.capture(self)
-            self.reinforce(march)
 
     def step(self):
         if self.owner:
@@ -162,99 +190,22 @@ class Fort:
     def __repr__(self):
         return self.name
 
-    def __eq__(self, other):
-        return self.name == other.name
-
-    def __lt__(self, other):
-        return self.name < other.name
-
     def __hash__(self):
-        return self.name.__hash__()
-
-
-class Road:
-    def __init__(self, ends):
-        self.positions = defaultdict(lambda: [])
-        self.start, self.end = ends
-        self.length = self.start.distance(self.end)
-
-    def add(self, march):
-        self.positions[march.pos()].append(march)
-
-    def remove(self, march):
-        self.positions[march.pos()].remove(march)
-
-    def marches(self):
-        return chain(*self.positions.values())
-
-    def step(self):
-        self.half_step()
-        self.resolve_encounters()
-        self.half_step()
-        self.resolve_encounters()
-        self.resolve_arrivals()
-
-    def half_step(self):
-        # the list() call is needed to fixate the current buckets
-        for march in list(self.marches()):
-            self.remove(march)
-            march.remaining_steps -= 0.5
-            self.add(march)
-
-    def resolve_encounters(self):
-        for bucket in self.positions.values():
-            for m1 in bucket:
-                for m2 in bucket:
-                    m1.encounter(m2)
-                    if m1.size <= 0:
-                        break
-
-    def resolve_arrivals(self):
-        for march in chain(self.positions[0], self.positions[self.length]):
-            march.arrive()
+        return hash(self.name)
 
 
 class March:
-    def __init__(self, game, origin, target, owner, size, steps):
-        self.game = game
-        self.origin = origin
-        self.target = target
+    def __init__(self, road, owner, size):
+        self.road = road
         self.owner = owner
         self.size = size
-        self.remaining_steps = steps
-        self.road().add(self)
         self.owner.marches.add(self)
 
-    def remove(self):
-        self.road().remove(self)
+    def die(self):
         self.owner.marches.remove(self)
 
-    def kill_soldiers(self, num):
-        self.size -= num
-        if self.size <= 0:
-            self.remove()
-
-    def encounter(self, other):
-        if self.owner != other.owner:
-            tmp = other.size
-            other.kill_soldiers(self.size)
-            self.kill_soldiers(tmp)
-
-    def arrive(self):
-        if self.owner == self.target.owner:
-            self.target.reinforce(self)
-        else:
-            self.target.besiege(self)
-
-    def road(self):
-        return self.game.roads[self.origin, self.target]
-
-    def pos(self):
-        if self.origin == min(self.origin, self.target):
-            return self.road().length - self.remaining_steps
-        else:
-            return self.remaining_steps
-
+    def __repr__(self):
+        return str(self.size)
 
 class Player:
     def __init__(self, game, name, cmd):
@@ -287,8 +238,8 @@ class Player:
 class Game:
     def __init__(self):
         self.forts = {}
+        self.roads = []
         self.players = {}
-        self.roads = UnorderedTupleDefaultDict(Road)
 
     def play(self):
         while not self.winner():
@@ -314,10 +265,9 @@ class Game:
             player.read_commands()
 
     def step(self):
-        for road in self.roads.values():
+        for road in self.roads:
             road.step()
-        for fort in self.forts.values():
-            fort.step()
+
 
 game = Game()
 Player(game, 'procrat', ['./procrat', 'test2'])
@@ -326,5 +276,7 @@ Player(game, 'iasoon', ['./procrat', 'test1'])
 with open(sys.argv[1], 'r') as handle:
     read_map(game, handle)
 
-game.play()
-print(game.winner().name)
+game.step()
+game.step()
+game.step()
+print(show_visible(game, game.forts.values()))
