9fb004ccbb1385e86a0cf6d3861410626a9f0ef0
Lorin Werthen <donvittoriomusic@gmail.com>
2016-02-18 00:46:37 +0100

    Added passenger integrationsi

---
+++ web/battlebots.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+from werkzeug.utils import secure_filename
+from os import path
+from markdown import Markdown
+
+app = Flask(__name__)
+
+@app.route('/battlebots/')
+@app.route('/battlebots')
+@app.route('/')
+def landing_page():
+    return docs('lorin_ipsum')
+
+@app.route('/docs/<name>')
+def docs(name):
+    md = Markdown(extensions=['markdown.extensions.toc'])
+    text = open(path.join('docs', secure_filename(name) + '.md')).read()
+    html = md.convert(text)
+    return render_template('doc.html', content=html, toc=md.toc)
+
+if __name__ == '__main__':
+    app.run()
