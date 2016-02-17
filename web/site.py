b9a1b8cf03e3f7b47c7ac8e319e0e228a6ede7a0
Ilion Beyst <ilion.beyst@gmail.com>
2016-02-17 19:12:22 +0100

    add header image n shit

--- web/site.py
+++ web/site.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
+from markdown import Markdown
+
 app = Flask(__name__)
 
 @app.route('/')
 def landing_page():
-    return render_template('landing.html')
+    md = Markdown(extensions=['markdown.extensions.toc'])
+    with open('content.md', 'r') as f:
+        text = f.read()
+    html = md.convert(text)
+    return render_template('landing.html', content=html, toc=md.toc)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- web/static/script.js
+++ web/static/script.js
@@ -1,7 +1,18 @@
-$(window).scroll(function(){
-  if ($(window).scrollTop() >= 300) {
-    $('nav').addClass('fixed-header');
-  } else {
-    $('nav').removeClass('fixed-header');
-  }
+$(document).ready(function() {
+   //cache selectors
+  var headers = $('.content h1');
+  var navbar_items = $('nav > .toc > ul > li');
+
+  $(window).scroll(function(){
+    if ($(window).scrollTop() >= window.innerHeight/3) {
+      $('#container').addClass('fixed-header');
+    } else {
+      $('#container').removeClass('fixed-header');
+    }
+    navbar_items.removeClass('focused');
+    var focused_id = headers.filter(function() {
+      return $(window).scrollTop() >= $(this).offset().top -1;
+    }).last().attr('id');
+    $('nav a[href="#'+ focused_id +'"]').parent().addClass('focused');
+  });
 });
