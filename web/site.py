9adf9d65e65bdab29fac244d2b8fc3b6711debd0
Ilion Beyst <ilion.beyst@gmail.com>
2016-02-17 23:42:31 +0100

    rendering docs is fun

--- web/site.py
+++ web/site.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template
+from werkzeug.utils import secure_filename
+from os import path
 from markdown import Markdown
 
 app = Flask(__name__)
 
 @app.route('/')
 def landing_page():
+    return docs('lorin_ipsum')
+
+@app.route('/docs/<name>')
+def docs(name):
     md = Markdown(extensions=['markdown.extensions.toc'])
-    with open('content.md', 'r') as f:
-        text = f.read()
+    text = open(path.join('docs', secure_filename(name) + '.md')).read()
     html = md.convert(text)
-    return render_template('landing.html', content=html, toc=md.toc)
+    return render_template('doc.html', content=html, toc=md.toc)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
