d5272743cee2b99bcec65f7d35b07f4ebb2b4e8e
Wout Schellaert <wout.schellaert@ugent.be>
2016-02-25 21:42:54 +0100

    Add basic logic to the Mind(tm)

--- testbots/weaksauce.py
+++ testbots/weaksauce.py
@@ -1,4 +1,4 @@
-from math import ceil, sqrt
+from math import ceil, floor, sqrt
 import sys
 
 MARCH_SPEED = 1
@@ -56,9 +56,9 @@ class March:
 
 
 class Game:
-    players = {}  # {name: player(Player)}
-    forts = {}    # {name: fort(Fort)}
-    marches = {}  # {origin: march(March)}
+    players = {}     # {name: player(Player)}
+    forts = {}       # {name: fort(Fort)}
+    marches = set()  # (march(March))
 
     @staticmethod
     def parse_fort(line):
@@ -78,15 +78,16 @@ class Game:
 
     @staticmethod
     def parse_march(line):
-        origin = line[0]
         owner_name = line[2]
         if owner_name not in Game.players.keys():
             Game.players[owner_name] = Player(owner_name)
 
-        Game.marches[origin] = March(*line)
+        Game.marches.add(March(*line))
 
     @staticmethod
     def start():
+        mind = Mind(ME)
+
         def handle(handler):
             for i in range(int(input().split()[0])):
                 handler(input().split())
@@ -96,8 +97,44 @@ class Game:
             handle(Game.parse_fort)
             handle(Game.parse_road)
             handle(Game.parse_march)
+            mind.play()
             print("Turn read")
 
 
+class Mind:
+    def __init__(self, name):
+        self.name = name
+        self.player = None
+        self.turn = 0
+        self.forts = {}            # {name: fort(Fort)}
+        self.marches = {}          # {destination(Fort): march(March)}
+        self.fort_safety = {}      # {fort(Fort): 0|1}
+        self.fort_threatened = {}  # {fort(Fort): 0|1}
+
+    def play(self):
+        self.__collect_data()
+        self.__defend_borders()
+        # TODO
+
+    def __collect_data(self):
+        self.player = Game.players[self.name]
+        self.forts = self.player.forts
+        self.marches = self.player.marches
+        self.fort_safety = {fort: 1 if self.__in_safety(fort) else 0 for fort in self.forts}
+
+    def __defend_borders(self):
+        for safe_fort in (fort for fort, safe in self.fort_safety.items() if safe):
+            half = floor(safe_fort.garrison / 2)
+            neighbour_num = len(safe_fort.neighbours)
+            amount = floor(neighbour_num)
+            for neighbour in safe_fort.neighbours.values():
+                # TODO
+                pass
+
+    def __in_safety(self, my_fort):
+        return all(fort.owner == self.player for fort in my_fort.neighbours.values())
+
+    def __threatened(self, my_fort):
+        return any(fort.owner != self.player for fort in my_fort.incoming_marches.values())
 Game.start()
 
