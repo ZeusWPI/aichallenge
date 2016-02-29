93d6cb354352f669c18bb28fa598adf6eb7d1160
Wout Schellaert <wout.schellaert@ugent.be>
2016-02-29 14:52:21 +0100

    Basic site template

--- web/battlebots.py
+++ web/battlebots.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from os import path
 from markdown import Markdown
@@ -6,9 +6,21 @@ from markdown import Markdown
 app = Flask(__name__)
 
 
+@app.route('/home')
 @app.route('/')
-def landing_page():
-    return docs('teaser')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 
 @app.route('/docs/<name>')
@@ -20,4 +32,4 @@ def docs(name):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
