634c8d98fe2fb56e3783bb8fd2f3be2ff91c3f0e
Stijn Seghers <seghers_stijn@hotmail.com>
2016-03-05 22:46:32 +0100

    Made dynamic navbar work again

--- web/static/script.js
+++ web/static/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   $(window).scroll(function() {
     if ($(window).scrollTop() >= window.innerHeight / 3) {
-      $('#container').addClass('fixed-header');
+      $('.navbar').addClass('navbar-fixed-top');
+      $('#below-navbar').addClass('fixed-header');
     } else {
-      $('#container').removeClass('fixed-header');
+      $('.navbar').removeClass('navbar-fixed-top');
+      $('#below-navbar').removeClass('fixed-header');
     }
   });
 });
