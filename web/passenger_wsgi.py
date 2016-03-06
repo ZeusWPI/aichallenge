39b91a4e181af00fce39077b65b57bc29f1b6ac1
Stijn Seghers <seghers_stijn@hotmail.com>
2016-03-06 10:55:50 +0100

    Fixed Passenger hack

--- web/passenger_wsgi.py
+++ web/passenger_wsgi.py
@@ -7,6 +7,6 @@ INTERP = os.path.expanduser("~/env/bin/python")
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 
-sys.path.append(os.getcwd())
+sys.path.append(os.path.dirname(os.getcwd()))
 
-from battlebots import app as application
+from web import app as application
