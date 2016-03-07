de65010a24b726ed41fbc547a6d18c8b73b083cd
Stijn Seghers <seghers_stijn@hotmail.com>
2016-03-07 19:55:34 +0100

    Code review 2.0

--- web/bots/forms.py
+++ web/bots/forms.py
@@ -1,18 +1,15 @@
 from flask.ext.wtf import Form
 from flask_wtf.file import FileField
-from wtforms import StringField, SubmitField, SelectMultipleField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length, Optional
 
 from web.forms import BOTNAME_LENTGH
 
 
 class BotForm(Form):
-    botname = StringField('Name', validators=[DataRequired(), Length(*BOTNAME_LENTGH)])
+    botname = StringField('Name', validators=[DataRequired(),
+                                              Length(*BOTNAME_LENTGH)])
     files = FileField('Files', validators=[DataRequired()])
     compile_cmd = StringField('Compile Command', validators=[Optional()])
     run_cmd = StringField('Run Command', validators=[DataRequired()])
     submit = SubmitField('Submit')
-
-
-class MultipleFileField(FileField):
-    pass
