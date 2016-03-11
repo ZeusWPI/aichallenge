774fca8d5a7ac3fb1cad482325d56a29066cb7c4
Stijn Seghers <seghers_stijn@hotmail.com>
2016-03-11 11:26:00 +0100

    It pains my heart

---
+++ battlebots/__init__.py
@@ -0,0 +1,9 @@
+import os
+
+# Since this package is just a bunch of separate but interdependable scripts,
+# but it's overkill to use distutils, we'll just make sure that the PYTHONPATH
+# is always correctly set. This is e.g. needed for werkzeug.
+# See https://github.com/mitsuhiko/werkzeug/issues/461#issuecomment-139369694
+# and https://github.com/mitsuhiko/flask/issues/1246
+__root_dir = os.path.dirname(os.path.dirname(__file__))
+os.environ['PYTHONPATH'] = __root_dir
