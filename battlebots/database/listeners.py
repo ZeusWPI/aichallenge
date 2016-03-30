b60dfc1d539e1bee6cb8122e071790a97066400e
Wout Schellaert <wout.schellaert@ugent.be>
2016-03-30 10:17:22 +0200

    Allow for draws

--- battlebots/database/listeners.py
+++ battlebots/database/listeners.py
@@ -4,12 +4,23 @@ from sqlalchemy import event
 
 from battlebots.database.models import Bot, K_FACTOR
 
+WIN_RESULT = 1
+LOSE_RESULT = 0
+DRAW_RESULT = 0.5
+
 
 @event.listens_for(Bot.matches, 'append')
 def new_match(bot, match, _):
-    result = int(match.winner == bot)
+    result = get_result(bot, match)
     opponents = match.participants - bot
     bot.score += change_amount(bot, opponents, result)
+
+
+def get_result(bot, match):
+    if not any(bot == match.winner for bot in match.participants):
+        return DRAW_RESULT
+
+    return WIN_RESULT if match.winner == bot else LOSE_RESULT
 
 
 def expected_result(a, b):
