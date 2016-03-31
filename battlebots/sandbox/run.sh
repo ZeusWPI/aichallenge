a692c61f4a3faefbede6ea3a59c871b6f9d6b691
Stijn Seghers <seghers_stijn@hotmail.com>
2016-03-31 17:08:38 +0200

    Make it a little bit more safe and working

--- battlebots/sandbox/run.sh
+++ battlebots/sandbox/run.sh
@@ -7,7 +7,17 @@ set -euo pipefail
 
 this_script_dir="$(dirname "$BASH_SOURCE")"
 bot_dir="$1"
+if [[ ! -d "${bot_dir}" ]]; then
+    echo "Please supply a directory as the first argument" >&2
+    exit 1
+fi
 shift
+
+unsafe_bot_script=$(mktemp -p "${bot_dir}" bot_XXXXX.sh)
+{
+    echo "$@"
+} >"${unsafe_bot_script}"
+chmod +x "${unsafe_bot_script}"
 
 # Set bot dir as user home dir and allow writes to it
 /usr/bin/firejail \
@@ -15,4 +25,6 @@ shift
     --private="${bot_dir}" \
     --whitelist="${bot_dir}" \
     -- \
-    "$@"
+    "$HOME/$(basename ${unsafe_bot_script})"
+
+rm "${unsafe_bot_script}"
