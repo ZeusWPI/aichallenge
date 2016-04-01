e1122ba1eed36ee232f288e132a25dfc87dbc182
Stijn Seghers <seghers_stijn@hotmail.com>
2016-04-01 14:53:22 +0200

    This is no longer used

--- battlebots/config.py
+++ battlebots/config.py
@@ -5,7 +5,6 @@ BASE_DIR = path.abspath(path.dirname(__file__))
 DB_DIR = path.join(BASE_DIR, 'database')
 BOT_CODE_DIR = path.join(BASE_DIR, 'bots')
 MATCH_LOG_DIR = path.join(BASE_DIR, 'logs')
-SANDBOX_CMD = path.join(BASE_DIR, 'sandbox', 'run.sh')
 
 SECRET_KEY = 'badass'
 LOG_LEVEL = logging.INFO
