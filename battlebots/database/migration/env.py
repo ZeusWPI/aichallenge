66a6f3f798c7f04e1dbcc107f90ebf2dedb881a8
Stijn Seghers <seghers_stijn@hotmail.com>
2016-04-02 10:36:35 +0200

    Add autogeneration support

--- battlebots/database/migration/env.py
+++ battlebots/database/migration/env.py
@@ -9,6 +9,7 @@ from pathlib import Path
 repo_root = Path(__file__).resolve().parents[3]
 sys.path.append(str(repo_root))
 import battlebots.config  # NOQA
+from battlebots.database import models  # NOQA
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,9 +23,7 @@ fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
