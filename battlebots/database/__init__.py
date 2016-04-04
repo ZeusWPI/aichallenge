ff904689446c81bf813f5cff1051b2e0a14c3d88
Stijn Seghers <seghers_stijn@hotmail.com>
2016-04-04 09:19:38 +0200

    Don't timeout connection with db

--- battlebots/database/__init__.py
+++ battlebots/database/__init__.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from battlebots import config
 
 
-engine = create_engine(config.SQLALCHEMY_DATABASE_URI)
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI, pool_recycle=3600)
 Session = sessionmaker(bind=engine, autoflush=False)
 session = Session()
 
