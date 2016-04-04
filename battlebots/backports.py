1b328d33ff668049f7cece39eca3d2326f14a31f
Stijn Seghers <seghers_stijn@hotmail.com>
2016-04-04 11:50:28 +0200

    Backport some more

--- battlebots/backports.py
+++ battlebots/backports.py
@@ -49,4 +49,37 @@ def sp_run(*popenargs, input=None, timeout=None, check=False, **kwargs):
         if check and retcode:
             raise sp.CalledProcessError(retcode, process.args, output=stdout,
                                         stderr=stderr)
-    return sp.CompletedProcess(process.args, retcode, stdout, stderr)
+    return CompletedProcess(process.args, retcode, stdout, stderr)
+
+
+class CompletedProcess(object):
+    """A process that has finished running.
+
+    This is returned by run().
+
+    Attributes:
+      args: The list or str args passed to run().
+      returncode: The exit code of the process, negative for signals.
+      stdout: The standard output (None if not captured).
+      stderr: The standard error (None if not captured).
+    """
+    def __init__(self, args, returncode, stdout=None, stderr=None):
+        self.args = args
+        self.returncode = returncode
+        self.stdout = stdout
+        self.stderr = stderr
+
+    def __repr__(self):
+        args = ['args={!r}'.format(self.args),
+                'returncode={!r}'.format(self.returncode)]
+        if self.stdout is not None:
+            args.append('stdout={!r}'.format(self.stdout))
+        if self.stderr is not None:
+            args.append('stderr={!r}'.format(self.stderr))
+        return "{}({})".format(type(self).__name__, ', '.join(args))
+
+    def check_returncode(self):
+        """Raise CalledProcessError if the exit code is non-zero."""
+        if self.returncode:
+            raise sp.CalledProcessError(self.returncode, self.args,
+                                        self.stdout, self.stderr)
