6defcdbe686c087e19f7c41582bef2b08f8f959d
Wout Schellaert <wout.schellaert@ugent.be>
2016-04-08 13:18:12 +0200

    Fix passenger

--- battlebots/web/passenger_wsgi.py
+++ battlebots/web/passenger_wsgi.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+from urllib.parse import unquote
 
 INTERP = os.path.expanduser("~/env/bin/python")
 if sys.executable != INTERP:
@@ -11,4 +12,9 @@ from pathlib import Path
 repo_root = Path(__file__).resolve().parents[2]
 sys.path.append(str(repo_root))
 
-from battlebots.web import app as application
+from battlebots.web import app
+
+
+def application(environ, start_response):
+    environ["PATH_INFO"] = unquote(environ["PATH_INFO"])
+    return app(environ, start_response)
