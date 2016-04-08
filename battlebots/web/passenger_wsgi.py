c1c0aabeb6c51715937aa7c3304edc5e4b87184f
Tom Naessens <siloxtom@gmail.com>
2016-04-08 18:27:17 +0200

    WIP: Special characters (#88)
    * Fix passenger
    
    * I am the passenger
    
    * Change order of imports

--- battlebots/web/passenger_wsgi.py
+++ battlebots/web/passenger_wsgi.py
@@ -8,7 +8,18 @@ if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 
 from pathlib import Path
+from urllib.parse import unquote
+
 repo_root = Path(__file__).resolve().parents[2]
 sys.path.append(str(repo_root))
 
 from battlebots.web import app as application
+
+wsgi_app = application.wsgi_app
+
+
+def _app(environ, start_response):
+    environ["PATH_INFO"] = unquote(environ["PATH_INFO"])
+    return wsgi_app(environ, start_response)
+
+application.wsgi_app = _app
