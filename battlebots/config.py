a877c37c4bc4bdcb178c9d208786a2f57328b947
Stijn Seghers <seghers_stijn@hotmail.com>
2016-04-08 12:47:30 +0200

    Use config.py again

--- battlebots/config.py
+++ battlebots/config.py
@@ -4,12 +4,14 @@ from os import path
 
 PRODUCTION = False
 
+# Paths
 BASE_DIR = path.abspath(path.dirname(__file__))
 REPO_ROOT = path.dirname(BASE_DIR)
 DB_DIR = path.join(BASE_DIR, 'database')
 BOT_CODE_DIR = path.join(BASE_DIR, 'bots')
 MATCH_LOG_DIR = path.join(BASE_DIR, 'matches')
 
+# Logs
 if PRODUCTION:
     LOG_LEVEL = logging.WARNING
     LOGS_HOME = path.join(os.environ['HOME'], 'log')
@@ -19,6 +21,7 @@ else:
 RANKER_LOG = path.join(LOGS_HOME, 'ranker.log')
 WEB_LOG = path.join(LOGS_HOME, 'web.log')
 
+# Yarr, here be secrets; secrets as deep as the sea
 SECRET_KEY = 'badass'
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + path.join(DB_DIR, 'database.db')
